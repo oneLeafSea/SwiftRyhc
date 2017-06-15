1d233c366815eb9ff521cbb0042c261534d8b612
冯学仕 <fengxueshi@fengxueshidemacbook-pro.local>
2017-06-15 14:10:43 +0800

    Initial Commit

---
+++ 珠海人员核查swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  珠海人员核查swift
+//
+//  Created by 冯学仕 on 17/6/15.
+//  Copyright © 2017年 rooten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
